57ff38735f93641e00f01cf16849717dd59cae0e
Bergson Lisboa Nóbrega <bergson.dev@outlook.com>
2026-05-01 09:45:45 -0400

    Initial Commit

---
+++ ContadorDeSkate/ContadorDeSkateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContadorDeSkateApp.swift
+//  ContadorDeSkate
+//
+//  Created by Bergson Lisboa Nobrega on 01/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ContadorDeSkateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContadorDeSkate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContadorDeSkate
+//
+//  Created by Bergson Lisboa Nobrega on 01/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
