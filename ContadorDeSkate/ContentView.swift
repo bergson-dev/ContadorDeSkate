9defe10d57afb7f546d3145abedd8d9b8d3548d4
Bergson Lisboa Nóbrega <bergson.dev@outlook.com>
2026-05-01 16:22:21 -0400

    Concluido o App Contador de Skate e a finalização das cores e adiconado os botões

--- ContadorDeSkate/ContentView.swift
+++ ContadorDeSkate/ContentView.swift
@@ -1,24 +1,40 @@
-//
-//  ContentView.swift
-//  ContadorDeSkate
-//
-//  Created by Bergson Lisboa Nobrega on 01/05/26.
-//
-
 import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
 
 #Preview {
     ContentView()
 }
+
+
+struct ContentView: View {
+    //Variavel que o swift "vigia". Se ela mudar, a tela muda!
+    @State var manobras = 0
+    
+    var body: some View { //Alinha tudo em cima do outro
+            
+            VStack(spacing: 20) {
+                Text("Treino de Hoje")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.blue)
+                
+                Text("\(manobras)") //Mostra o valor da variável
+                    .font(.largeTitle)
+                    .foregroundColor(manobras >= 5 ? .pink : .primary)
+                
+                Button("Acertei uma Manobra! 🛹") {
+                    manobras = manobras + 1 // Ação do botão
+
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.green)
+                
+                Button("Zerar Contador") {
+                    manobras = 0  // Isso volta o valor para zero
+                    
+                }
+                .buttonStyle(.bordered)
+                .tint(.red)
+                .padding(.top, 10)
+            }
+        }
+    }
